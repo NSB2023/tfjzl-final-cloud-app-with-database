f41f578de121f5384cf8229e28cf99b78f1f3ea9
NSB2023 <sakibscl@gmail.com>
2026-02-14 09:39:44 -0500

    Completed exam submission and result feature with screenshots

--- myproject/urls.py
+++ myproject/urls.py
@@ -13,12 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import include, path
+# from django.conf.urls.static import static
+# from django.conf import settings
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('onlinecourse/', include('onlinecourse.urls')),
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 from django.contrib import admin
-from django.urls import include, path
-from django.conf.urls.static import static
-from django.conf import settings
+from django.urls import path, include   # ✅ ADD path and include
+from django.shortcuts import redirect   # ✅ you already added this
 
 urlpatterns = [
+    path('', lambda request: redirect('onlinecourse:index')),
     path('admin/', admin.site.urls),
     path('onlinecourse/', include('onlinecourse.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
